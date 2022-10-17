ed69027b48d5f75f6628e3eda18723eea857f703
fabriziodelisa <fabrizio.de.lisa@gmail.com>
2022-10-17 19:45:10 -0300

    load of full data finish (funtional PUT)

--- src/pages/EnterpriseData.js
+++ src/pages/EnterpriseData.js
@@ -1,33 +1,43 @@
-import React, { useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import EnterpriseDataPag1 from "../components/forms/enterpriseRegisterForms/EnterpriseDataPag1";
 import EnterpriseDataPag2 from "../components/forms/enterpriseRegisterForms/EnterpriseDataPag2";
+import UserContext from "../context/UserContext"
 
 const EnterpriseData = () => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState({});
   const [boolPage2, setBoolPage2] = useState(false);
   const [successfulCharge, setSuccessfulCharge] = useState(false);
+  const {jwt} = useContext(UserContext);
 
-  if (successfulCharge) {
-    //Cuando este listo el crud del back tengo que:
-    //poner un set time out de 5 seg para dejar que se terminen de mostrar las alertas
-    //luego hacer el fetch
-    //Y en el then usar el Navigate a Home.
-    
-    console.log("carga correcta ", successfulCharge);
-    console.log(data);
+
+  const UpdateData= (datos) =>{
+    const _datos = {...data, ...datos}
+     setData(_datos);
   }
+
+  useEffect(() => {
+    fetch("https://localhost:7172/api/UsersInfo/CreateDataCompany", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [data, jwt])
+  
 
   return !boolPage2 ? (
     <div>
-      <EnterpriseDataPag1 setBoolPage2={setBoolPage2} setData={setData} />
+      <EnterpriseDataPag1 UpdateData={UpdateData} setBoolPage2={setBoolPage2}  />
     </div>
   ) : (
     <div>
-      <EnterpriseDataPag2
-        data={data}
-        setData={setData}
-        setSuccessfulCharge={setSuccessfulCharge}
-      />
+      <EnterpriseDataPag2 UpdateData={UpdateData} setSuccessfulCharge={setSuccessfulCharge} />
     </div>
   );
 };
