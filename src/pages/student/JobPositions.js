15e780e92bc53b4cf5214df5db32ba38ab0dba14
Tomas Sacripanti <tsacripanti@primerosystems.com>
2022-11-09 17:41:18 -0300

    Remove footer

--- src/pages/student/JobPositions.js
+++ src/pages/student/JobPositions.js
@@ -238,28 +238,6 @@ const JobPositions = () => {
             </div>
           </div>
         </div>
-        <footer className="footerCompany">
-          <div id="divFooter" className="container">
-            <div id="divLeftRight" className="row justify-content-center">
-              <div id="divLeft" className="col-4">
-                <figure>
-                  <img src={utnLogo} alt="UTN Logo" className="logo" />
-                </figure>
-              </div>
-              <div id="divRight" className="col-4">
-                <div className="divUniversity">
-                  <p>FACULTAD REGIONAL ROSARIO</p>
-                </div>
-                <div className="divContact">
-                  <p> Localidad: Zeballos 1341 - Rosario</p>
-                </div>
-                <div className="divPhone">
-                  <p>Telefono: 0341-4481871</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
       </div>
       <footer className="footerStudentUniversity">
         <div id="divFooter" className="container">
